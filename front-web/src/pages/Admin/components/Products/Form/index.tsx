b2f264b4d9bf1d4c509118ec9eef8b762bcf5f63
Lucas Figueiredo Bof <lucasfbof@gmail.com>
2021-01-08 05:25:15 -0300

    Add toast when save product

--- front-web/src/pages/Admin/components/Products/Form/index.tsx
+++ front-web/src/pages/Admin/components/Products/Form/index.tsx
@@ -1,8 +1,10 @@
-import { makePrivateRequest } from 'core/utils/request';
 import React from 'react';
+import { makePrivateRequest } from 'core/utils/request';
 import { useForm } from 'react-hook-form';
+import { toast } from 'react-toastify';
 import BaseForm from '../../BaseForm';
 import './styles.scss';
+import { useHistory } from 'react-router-dom';
 
 type FormState = {
     name: string;
@@ -13,9 +15,17 @@ type FormState = {
 
 const Form = () => {
     const { register, handleSubmit, errors } = useForm<FormState>();
+    const history = useHistory();
 
     const onSubmit = (data: FormState) => {
-        makePrivateRequest({ url: '/products', method: 'POST', data });
+        makePrivateRequest({ url: '/products', method: 'POST', data })
+            .then(() => {
+                toast.info('Produto cadastrado com sucesso!');
+                history.push('/admin/products');
+            })
+            .catch(() => {
+                toast.error('Erro ao salvar produto!');
+            });
     }
 
     return (
